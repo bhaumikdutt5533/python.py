093ed87a67e3772ed7a03d3298177d0e604614a9
Raj Khaitan <rajkhaitan3108@gmail.com>
2025-08-23 13:15:39 +0530

    Create p1.py

---
+++ p1.py
@@ -0,0 +1,14 @@
+import random
+
+print(" Welcome to Number Guessing Game ")
+
+# computer picks a random number between 1 and 10
+number = random.randint(1, 10)
+
+# ask the player to guess
+guess = int(input("Guess a number between 1 and 10: "))
+
+if guess == number:
+    print("Congratulations! You guessed it right!")
+else:
+    print(f" Oops! The correct number was {number}")
